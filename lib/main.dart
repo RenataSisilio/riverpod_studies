857e90a366b846c5c85c6475fe51f7dc2a3457f5
Renata Sisilio <renatamelosisiliosilva@gmail.com>
2024-10-04 10:01:50 -0300

    refactor: move home module to modules folder (to fit the incoming architecture)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'home/home_page.dart';
+import 'app/modules/home/home_page.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
